bfdfe627ba1afcc006a67317dd799fa2da3b0adc
Sean Butlin <sean.butlin@boohoo.com>
2023-01-30 11:03:32 +0000

    second commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const products = require('./routes/productRoute');
+const products = require('../routes/productRoute');
 
 app.use(express.json());
 
